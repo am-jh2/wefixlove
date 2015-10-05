4f501d1d2625d52164007bec80c97a6d874e5244
am-jh2 <jonathan@ahltorpmedia.se>
2015-10-05 14:57:35 +0200

    Bugfix syntax error

--- no_love.php
+++ no_love.php
@@ -1,6 +1,10 @@
 <?php
+    $user = USER_NAME;
+
     if($user == 'Björn'){
         return 'no_love';
+    }elseif($user == 'Arto'){
+        return 'Finland vodka';
     }else{
         return 'love';
     }
